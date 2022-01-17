01d71620a4e42d126d0fda3e551daad9d0974ad8
ajhosan <alexjhosan19@gmail.com>
2022-01-17 11:30:41 +0700

    route

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import FirstPage from './pages/FirstPage/FirstPage.pages';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Login from './components/navbar/navbar.component';
 
 function App() {
   return (
-    <div className="App">
-      <FirstPage></FirstPage>
-      <h1>{process.env.REACT_APP_API_URL}</h1>
-      <h1>{process.env.REACT_APP_API_KEY}</h1>
+    // <div className="App">
+    //   <FirstPage></FirstPage>
+
+    //   <h1>{process.env.REACT_APP_API_URL}</h1>
+    //   <h1>{process.env.REACT_APP_API_KEY}</h1>
+    // </div>
+    <div maxWidth="sm">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<FirstPage></FirstPage>} />
+          <Route path="/login" element={<Login/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
