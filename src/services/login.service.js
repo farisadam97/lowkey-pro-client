8800d8ea6cf8f2a018f490288a7ed84566af322c
ajhosan <alexjhosan19@gmail.com>
2022-01-23 17:10:57 +0700

    local storage

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -11,10 +11,16 @@ export const postLoginAxios = async (bodyReq) => {
           })
           .then(function (response) {
             console.log(response.data);
-            localStorage.setItem('token', response.data.message);
+            localStorage.setItem('bio', response.data.bio);
+            localStorage.setItem('city', response.data.city);
+            localStorage.setItem('email', response.data.email);
+            localStorage.setItem('name', response.data.name);
+            localStorage.setItem('token', response.data.token);
+            localStorage.setItem('status', "Login Success");
+            window.location = "/";
           })
           .catch(function (error) {
-            console.log(error.response.data);
+            alert(error.response.data.message);
           });
     }catch(error){
         console.log(error)
