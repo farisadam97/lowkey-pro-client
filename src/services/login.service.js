d0afe10575977f2aa3ee23f1b9412beaf78e0311
Alex Jhosan <alexjhosan19@gmail.com>
2022-01-24 09:36:03 +0700

    update redirect url after login success

--- src/services/login.service.js
+++ src/services/login.service.js
@@ -11,13 +11,14 @@ export const postLoginAxios = async (bodyReq) => {
           })
           .then(function (response) {
             console.log(response.data);
+            localStorage.setItem('id', response.data.id);
             localStorage.setItem('bio', response.data.bio);
             localStorage.setItem('city', response.data.city);
             localStorage.setItem('email', response.data.email);
             localStorage.setItem('name', response.data.name);
             localStorage.setItem('token', response.data.token);
             localStorage.setItem('status', "Login Success");
-            window.location = "/";
+            window.location = "/home-page";
           })
           .catch(function (error) {
             alert(error.response.data.message);
