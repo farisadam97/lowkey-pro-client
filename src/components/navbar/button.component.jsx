4e5f46ca36400d015c06135b5252cfade76fd8f2
ajhosan <alexjhosan19@gmail.com>
2022-01-25 14:06:01 +0700

    navbar fix

---
+++ src/components/navbar/button.component.jsx
@@ -0,0 +1,51 @@
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import { Link } from 'react-router-dom';
+
+
+
+
+
+export const ButtonNavbar = () =>{
+
+    const onLogoutClick =  (e) => {
+        e.preventDefault();
+        window.localStorage.clear();
+        alert("Anda telah logout");
+    }
+    const isUserLogin = localStorage.getItem('status');
+    return (
+        <>
+            {
+              isUserLogin === "Login Success" ? 
+              (
+                <>
+                  <Button variant="contained"> 
+                    <Link style={{ textDecoration: 'none', color: '#fff' }} to="/home-page">
+                      My Profile
+                    </Link>
+                  </Button>
+                  <Button variant="contained" color="error" onClick={onLogoutClick}> 
+                    <Link style={{ textDecoration: 'none', color: '#fff' }} to="/login">
+                      Logout
+                    </Link>
+                  </Button>
+                </>
+              ) : (
+                <>
+                  <Button variant="contained"> 
+                  <Link style={{ textDecoration: 'none', color: '#fff' }} to="/register">
+                    Sign In
+                  </Link>
+                  </Button>
+                  <Button variant="contained">
+                    <Link style={{ textDecoration: 'none', color: '#fff' }} to="/login">
+                      Login
+                    </Link>
+                  </Button>
+                </>
+              ) 
+            }
+        </>
+    )
+}
